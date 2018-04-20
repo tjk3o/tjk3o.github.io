b946b4a3b071696c906dea31a69dfe5422445a2c
Tom Keogh <thomas.keogh@me.com>
2018-04-20 13:51:12 +0100

    Videos changed for gifs, and project container styling amended

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ $(() => {
     const target = this.hash;
     const $target = $(target);
     $('html, body').stop().animate({
-      'scrollTop': $target.offset().top
+      'scrollTop': ($target.offset().top) - 80
     }, 900, 'swing', function () {
       window.location.hash = target;
     });
@@ -27,7 +27,7 @@ $(() => {
     'For my second project I decided to make an Instagram clone. The real challenge came in restricting access to certain actions on the site, so that, for example, you could only delete your own photos. I found it immensely satisfying successfully implementing the Filestack API.',
     'CRAVE satisfies your cravings on an A to B route by displaying restaurants, within a set radius, that cater to your particular mood. As we had a tight deadline we worked under an agile methodology. During morning stand ups we would show what we had been working on to ensure we all knew how the app worked. I focused mainly on the AngularJS 1 front end, and I’m particularly proud of the discreet animated navigation menu that I implemented.',
     'For my final project I decided to focus on a mobile first experience, delivering a competitive todo list for different content. I used React on the front end and Node.js on the back end. I integrated the Spotify and TMDB APIs on the back end, to fetch and display relevant imagery and metadata to the user. Insomnia was helpful for checking API calls, and testing was an effective way of ensuring the efficacy of my requests. I used Trello so that I would have an accurate impression of how long I was able to spend on tasks and to prioritise them sensibly.'];
-  const projectVideos = ['maze-explorer.mov', 'tomstagram.mov', 'crave.mov', 'content.mov'];
+  const projectDemos = ['maze-explorer.gif', 'tomstagram.gif', 'crave.gif', 'content.gif'];
   const blurbContainer = $('.project-blurb');
   const demoContainer = $('.project-demo-container');
 
@@ -38,7 +38,7 @@ $(() => {
       .append( `<h1 class="">${projectTitles[e.target.value]}</h1>` )
       .append( `<p class="">${projectBlurbs[e.target.value]}</p>` );
     demoContainer
-      .append( `<video src="videos/${projectVideos[e.target.value]}" autoplay loop poster=""></video>` );
+      .append( `<img src="images/${projectDemos[e.target.value]}" />` );
   }
 
   projectShowBtn.on('click', (e) => selectProject(e));
